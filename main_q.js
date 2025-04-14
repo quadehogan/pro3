6da302264e4489f9ebcec4a1146991a9d7706eaf
quadehogan <qhogan@byu.edu>
2025-04-14 14:56:59 -0600

    after_class

--- main_q.js
+++ main_q.js
@@ -6,4 +6,21 @@ fetch("https://music.is120.ckearl.com/")
 
 function completeSteps(dataObject) {
     console.log(dataObject)
+    genreSectionDevelopment(dataObject)
+}
+
+function genreSectionDevelopment(dataObject){
+    let genres = dataObject["spotify_top_genre_artists"];
+    console.log(genres);
+
+    for (let genreObject of genres) {
+        let name = genreObject.genre_name;
+        document.getElementById(name).innerHTML = name;
+
+        let artists = genreObject["artists"]
+        for (let artistIndex in artists) {
+            artist = artists[artistIndex];
+            console.log(artist.name)
+        }
+    }
 }
