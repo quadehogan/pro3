38a2e9ebcb6070f029ce5429967460678233c6ad
quadehogan <qhogan@byu.edu>
2025-04-15 20:33:36 -0600

    images_done

--- main_q.js
+++ main_q.js
@@ -1,6 +1,14 @@
+const spinner = document.getElementById("spinner");
+
+spinner.style.display = "block";
+
+
 fetch("https://music.is120.ckearl.com/")
 	.then((response) => response.json())
 	.then((dataObject) => {
+
+        spinner.style.display = "none";
+        
 		completeSteps(dataObject["data"]);
 	});
 
@@ -15,12 +23,50 @@ function genreSectionDevelopment(dataObject){
 
     for (let genreObject of genres) {
         let name = genreObject.genre_name;
-        document.getElementById(name).innerHTML = name;
+        let genreName = document.getElementById(name)
+        genreName.innerHTML = `${name} artists`;
+        genreName.style.color = "var(--secondary-color)"
+        genreName.style.fontSize = "1.5rem"
 
         let artists = genreObject["artists"]
         for (let artistIndex in artists) {
             artist = artists[artistIndex];
-            console.log(artist.name)
+            let artistNameSection = document.createElement("div")
+            let artistName = document.createElement("p")
+            let artistImage = document.createElement("img")
+            let spotifyURL = artist.spotify_url
+            let spotifyLink = document.createElement("a")
+
+            artistName.innerHTML = artist.name
+            artistNameSection.appendChild(artistName)
+
+            artistImage.src = artist.image
+            artistImage.alt = `${artist.name}_image`
+            artistImage.classList.add("artist_image")
+            artistImage.classList.add("hover")
+
+            spotifyLink.href = spotifyURL
+            spotifyLink.target = "_blank"
+            spotifyLink.appendChild(artistImage)
+
+            artistNameSection.appendChild(spotifyLink)
+            artistNameSection.style.borderRadius = "0.5rem"
+            artistNameSection.style.margin = "1rem"
+            artistNameSection.style.marginTop = "0.3rem"
+            artistNameSection.style.display = "flex"
+            artistNameSection.style.flexDirection = "column"
+            artistNameSection.style.backgroundColor = "var(--main-color-sl)"
+            artistNameSection.style.padding = "1rem"
+
+            artistName.style.color = "var(--main-color-sl)"
+            artistName.style.alignSelf = "center"
+            artistName.style.color = "var(--secondary-color)"
+            artistName.style.fontSize = "1.1rem"
+            artistName.style.paddingTop = "0.1rem"
+            artistName.style.marginTop = "0.1rem"
+
+            let artistsSections = document.getElementById(`${name}_artists`)
+            artistsSections.appendChild(artistNameSection)
         }
     }
 }
